705f37a166710e89c5fad9bb43888b656e561ba8
EC2 Default User <ec2-user@ip-172-31-15-162.eu-west-3.compute.internal>
2020-02-11 21:03:44 +0000

    Cambios en pro

--- Configuration/DataBaseConfig.js
+++ Configuration/DataBaseConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-	host: 'localhost',
+	host: 'wanderplaydev.cfebmmc7xweo.eu-west-3.rds.amazonaws.com',
 	user: 'mikel',
-	password: 'mikel',
-	database: 'WanderPlayDev'
+	password: 'Wanderplay2020',
+	database: 'WanderPlay'
 }
