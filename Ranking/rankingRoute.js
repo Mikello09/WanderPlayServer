ace4f65fb9b9c0f5b616fbddeef6abb1ab9b7bc1
Codeboudns <codebounds@gmail.com>
2020-09-19 23:35:51 +0200

    ranking refactor to mongo

--- Ranking/rankingRoute.js
+++ Ranking/rankingRoute.js
@@ -1,92 +1,29 @@
 'use strict';
 
 const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const util = require('util');
-const dataBaseConfig = require('../Configuration/DataBaseConfig');
-const proxy = require('../Configuration/Proxy');
-
-
 const api = express.Router();
-const connection = mysql.createConnection({
-    host: dataBaseConfig.host,
-    user: dataBaseConfig.user,
-    password: dataBaseConfig.password,
-    database: dataBaseConfig.database,
-    multipleStatements: true
-});
+const bodyParser = require('body-parser');
+const databaseConfig = require('../Configuration/DataBaseConfig');
+const proxy = require('../Configuration/Proxy');
+const mongoose = require('mongoose');
 
-class RankingClass{
-    constructor(){}
-    set idUsuario(idUsuario){
-        this._idUsuario = idUsuario;
-    }
-    get idUsuario(){
-        return this._idUsuario;
-    }
-    set nombre(nombre){
-        this._nombre = nombre;
-    }
-    get nombre(){
-        return this._nombre;
-    }
-    set puntos(puntos){
-        this._puntos = puntos;
-    }
-    get puntos(){
-        return this._puntos;
-    }
-    set lugaresVisitados(lugaresVisitados){
-        this._lugaresVisitados = lugaresVisitados;
-    }
-    get lugaresVisitados(){
-        return this._lugaresVisitados;
-    }
-    set avatar(avatar){
-        this._avatar = avatar;
-    }
-    get avatar(){
-        return this._avatar;
-    }
-}
+
+
 
 api.use(bodyParser.urlencoded({extended:false}))//necesario para parsear las respuestas en el body
 
-const query = util.promisify(connection.query).bind(connection);//para hacer las llamadas con await
 
 api.post('/getGeneralRanking', async(req,res) => {
     if(proxy.isUserAuthenticated(req.headers['authtoken'])){
         try{
-            var rankings = [];
-            console.log("getGeneralRanking")
-            var usuarios = await query("SELECT * FROM Usuario");
-            console.log(usuarios);
-            for(var i=0;i<usuarios.length;i++){
-                var rank = new RankingClass()
-                rank.idUsuario = usuarios[i].idUsuario;
-                rank.nombre = usuarios[i].Nombre;
-                rank.puntos = usuarios[i].Puntos;
-                rank.avatar = usuarios[i].Avatar;
-                var lugaresVisitados = await query("SELECT * FROM Visitas WHERE Usuario_idUsuario = ?;", usuarios[i].idUsuario);
-                rank.lugaresVisitados = lugaresVisitados.length
-                rankings.push(rank)
-            }
-            var data = {
-                "state":"OK",
-                "data":rankings
-            }
-            res.json({data});
+            const Usuario = mongoose.model('Usuario', databaseConfig.usuarioSchema);
+            const usuarios = await Usuario.find({});
+            res.status(200).json({usuarios});
         }catch (err){
-            console.log("Error", err);
-			var data = {
-				"state":"SQLError",
-				"reason":err
-			};
-			res.json({data});
+            res.status(500).json({"reason":err});
 		}
     } else {
-        res.json({"state":"Unauthorized"});
+        res.status(401).json({"state":"Unauthorized"});
     }
 });
 
